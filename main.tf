aba23a4b18cd46e582d3b46409324eda6fbb2291
Yulei Liu <yulei.liu@gmail.com>
2021-11-04 11:35:36 +1100

    change to smaller instance

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "e2-standard-2"
+  machine_type = "e2-small"
       zone = "australia-southeast1-a"
   // zone         = "us-central1-a"
 
@@ -18,22 +18,22 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-resource "google_compute_instance" "vm_instance1" {
-  name         = "terraform-instance1"
-  machine_type = "e2-medium"
-      zone = "australia-southeast1-a"
-  // zone         = "us-central1-a"
+# resource "google_compute_instance" "vm_instance1" {
+#   name         = "terraform-instance1"
+#   machine_type = "e2-medium"
+#       zone = "australia-southeast1-a"
+#   // zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = "default"
+#     access_config {
+#     }
+#   }
+# }
