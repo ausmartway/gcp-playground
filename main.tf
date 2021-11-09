d781354f313fa49807adac37a731413b6711fb35
Yulei Liu <yulei.liu@gmail.com>
2021-11-10 09:15:44 +1100

    GKE cluster

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-small"
-      zone = "australia-southeast1-a"
-      allow_stopping_for_update = true
-  // zone         = "us-central1-a"
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "terraform-instance"
+#   machine_type = "e2-small"
+#       zone = "australia-southeast1-a"
+#       allow_stopping_for_update = true
+#   // zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = "default"
+#     access_config {
+#     }
+#   }
+# }
 
 # resource "google_compute_instance" "vm_instance1" {
 #   name         = "terraform-instance1"
