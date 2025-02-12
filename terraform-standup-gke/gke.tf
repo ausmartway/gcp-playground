8016a9c1b4230bb7ab454bd2715ee775e1909246
Yulei Liu <yulei.liu@gmail.com>
2025-02-12 18:32:10 +1100

    remove unnessisary resources

--- terraform-standup-gke/gke.tf
+++ terraform-standup-gke/gke.tf
@@ -1,7 +1,7 @@
-resource "google_service_account" "default" {
-  account_id   = "service-account-for-nodepool"
-  display_name = "Service Account for nodepool"
-}
+# resource "google_service_account" "default" {
+#   account_id   = "service-account-for-nodepool"
+#   display_name = "Service Account for nodepool"
+# }
 
 resource "google_container_cluster" "primary" {
   name     = "playground"
@@ -15,32 +15,32 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "playground-node-pool"
-  location   = "australia-southeast1"
-  cluster    = google_container_cluster.primary.name
-  initial_node_count = 1
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 3
-  }
+# resource "google_container_node_pool" "primary_preemptible_nodes" {
+#   name       = "playground-node-pool"
+#   location   = "australia-southeast1"
+#   cluster    = google_container_cluster.primary.name
+#   initial_node_count = 1
+#   autoscaling {
+#     min_node_count = 0
+#     max_node_count = 3
+#   }
 
-  management {
-    auto_repair = true
-    auto_upgrade = true
-  }
+#   management {
+#     auto_repair = true
+#     auto_upgrade = true
+#   }
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
+#   node_config {
+#     preemptible  = true
+#     machine_type = "e2-medium"
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     service_account = google_service_account.default.email
+#     oauth_scopes    = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
 
 # resource "google_service_account" "dns01-solver" {
 #   account_id   = "dns01-solver"
